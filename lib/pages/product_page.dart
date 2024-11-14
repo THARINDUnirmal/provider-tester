b9989e2d3c5d96d7082ed142598d3a1e53076a4f
Tharindu Nirmal <155609199+THARINDUnirmal@users.noreply.github.com>
2024-11-14 09:15:23 +0530

    project initial

---
+++ lib/pages/product_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:provider_tester/data/product_data.dart';
+import 'package:provider_tester/models/product_model.dart';
+
+class ProductPage extends StatelessWidget {
+  ProductPage({super.key});
+
+  final List<ProductModel> allProduct = ProductData().productList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Flutter Shop",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.deepOrange,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: allProduct.length,
+        itemBuilder: (context, index) {
+          final ProductModel indexProdut = allProduct[index];
+          return Card(
+            child: ListTile(
+              title: Row(
+                children: [
+                  Text(
+                    indexProdut.title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+              subtitle: Text(
+                "${indexProdut.price.toString()} \$",
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.shopping_cart_outlined,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.deepOrange,
+            onPressed: () {},
+            child: const Center(
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.deepOrange,
+            onPressed: () {},
+            child: const Center(
+              child: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
